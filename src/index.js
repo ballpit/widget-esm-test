cd46a1e39ebf876f319c5b5534bc65e2f0bab7f0
Bret Comnes <bcomnes@gmail.com>
2018-01-05 19:09:48 -0800

    Use beta version

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import * as netlifyIdentity from 'netlify-identity-widget';
-
-debugger;
+import netlifyIdentity from 'netlify-identity-widget';
 
 window.netlifyIdentity = netlifyIdentity
 // You must run this once before trying to interact with the widget
 netlifyIdentity.init()
-
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
