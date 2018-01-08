d30a4ecab3d8912d6f7518932cdaefec01725233
Bret Comnes <bcomnes@gmail.com>
2018-01-08 10:53:51 -0800

    open widget

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import netlifyIdentity from 'netlify-identity-widget';
 window.netlifyIdentity = netlifyIdentity
 // You must run this once before trying to interact with the widget
 netlifyIdentity.init()
+netlifyIdentity.open()
