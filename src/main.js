56051c90d69918ff428e83167f15c27f3b2e2e93
Bret Comnes <bcomnes@gmail.com>
2018-01-08 13:40:40 -0800

    Load before

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import netlifyIdentity from 'netlify-identity-widget'
+window.netlifyIdentity = netlifyIdentity
+netlifyIdentity.init({
+  container: '#widgets'
+})
 // === DEFAULT / CUSTOM STYLE ===
 // WARNING! always comment out ONE of the two require() calls below.
 // 1. use next line to activate CUSTOM STYLE (./src/themes)
@@ -24,11 +29,6 @@ import 'quasar-extras/material-icons'
 // import 'quasar-extras/ionicons'
 // import 'quasar-extras/fontawesome'
 // import 'quasar-extras/animate'
-import netlifyIdentity from 'netlify-identity-widget'
-window.netlifyIdentity = netlifyIdentity
-netlifyIdentity.init({
-  container: '#widgets'
-})
 
 Quasar.start(() => {
   /* eslint-disable no-new */
